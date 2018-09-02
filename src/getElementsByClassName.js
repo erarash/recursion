8b00200c4e2289eb2faca36960ef8f1f883fcafc
Arash <erani.arash@gmail.com>
2018-09-01 19:37:51 -0700

    finished recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  return getElementsByClassNameHelper(document.body, className);
+};
+
+var getElementsByClassNameHelper = function(node, className) {
+  const elements = [];
+  if (node instanceof Element) {
+    if (node.classList.contains(className)) {
+      elements.push(node);
+    }
+  }
+
+  node.childNodes.forEach(function(childNode) {
+    const childElements = getElementsByClassNameHelper(childNode, className);
+    childElements.forEach(function(childElement) {
+      elements.push(childElement);
+    });
+  });
+
+  return elements;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,41 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (obj === true) {
+    return "true";
+  } else if (obj === false) {
+    return "false";
+  }
+  if (typeof obj === "number") {
+    return obj.toString();
+  }
+  if (obj === undefined) {
+    return "";
+  }
+  if (obj === null) {
+    return "null";
+  }
+  if (typeof obj === "string") {
+    return `"${obj}"`;
+  }
+  if (Array.isArray(obj)) {
+    var stringList = "";
+    for (var i = 0; i < obj.length; i++) {
+      stringList += stringifyJSON(obj[i]);
+      if (i !== obj.length - 1) {
+        stringList += ",";
+      }
+    }
+    return `[${stringList}]`;
+  }
+  if (typeof obj === "object") {
+    stringList = "";
+    for (var key in obj) {
+      if (key !== "undefined" && key !== "functions") {
+        stringList += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+      }
+    }
+    stringList = stringList.substring(0, stringList.length - 1);
+    return `{${stringList}}`;
+  }
 };
